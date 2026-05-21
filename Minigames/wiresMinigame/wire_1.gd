f59ee7c4931f8ca9eb1671bcbdaffa14a4dc101b
williammo29 <william2011mo@gmail.com>
2026-05-21 10:42:40 -0400

    Update wire_1.gd

--- Minigames/wiresMinigame/wire_1.gd
+++ Minigames/wiresMinigame/wire_1.gd
@@ -10,14 +10,14 @@ func _ready() -> void:
 	polygon.color = Color.RED
 	hitbox.input_event.connect(_on_area_2d_input_event);
 	#.position is it relative to the parent node hence the -global_position
-	hitbox.position = to_local(freeEnd) - Vector2(150, 0);
+	hitbox.position = to_local(freeEnd) - Vector2(300, 0);
 	updateWire();
 
 func _process(_delta: float) -> void:
 	if(isDragging):
 		freeEnd = get_global_mouse_position();
 		updateWire();
-		hitbox.position = to_local(freeEnd) - Vector2(150, 0);
+		hitbox.position = to_local(freeEnd) - Vector2(300, 0);
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			isDragging = false
 	
@@ -33,6 +33,7 @@ func updateWire():
 		to_local(freeEnd - perp),
 		to_local(freeEnd + perp)
 		];
+	hitbox.position = to_local(freeEnd) - Vector2(150, 0);
 
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
