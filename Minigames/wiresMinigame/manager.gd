72e917a2ec555dd2ca1bec0ef92754048e72a20a
williammo29 <william2011mo@gmail.com>
2026-05-26 23:21:32 -0400

    randomized the wires

--- Minigames/wiresMinigame/manager.gd
+++ Minigames/wiresMinigame/manager.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 const Wire = preload("res://Minigames/wiresMinigame/wire_1.tscn")
 var wires = []
-
+var startPos = [150, 400, 650, 900];
+var endPos = [160, 430, 700, 950];
 func _ready() -> void:
+	startPos.shuffle(); 
+	endPos.shuffle();
 	#for some reason the targetPos is a bit off so I'm adjusting it a little
-	spawnWire(Vector2(100, 150), Vector2(500, 150), Vector2(1700, 160), Color.YELLOW)
-	spawnWire(Vector2(100, 650), Vector2(500, 650), Vector2(1700, 700), Color.BLUE)
-	spawnWire(Vector2(100, 900), Vector2(500, 900), Vector2(1700, 950), Color.GREEN)
+	spawnWire(Vector2(100, startPos[0]), Vector2(500, startPos[0]), Vector2(1700, endPos[0]), Color.YELLOW)
+	spawnWire(Vector2(100, startPos[1]), Vector2(500, startPos[1]), Vector2(1700, endPos[1]), Color.RED)
+	spawnWire(Vector2(100, startPos[2]), Vector2(500, startPos[2]), Vector2(1700, endPos[2]), Color.BLUE)
+	spawnWire(Vector2(100, startPos[3]), Vector2(500, startPos[3]), Vector2(1700, endPos[3]), Color.GREEN)
 
 #just makes a bunch of other wires
 func spawnWire(anchorPos: Vector2, startPosition: Vector2, targetPos: Vector2, wColor: Color):
