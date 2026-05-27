9d691ea0cc9870a44adbeadd193728fc84739e60
williammo29 <william2011mo@gmail.com>
2026-05-26 23:33:35 -0400

    finished the minigame manager panel

--- minigameAccess.gd
+++ minigameAccess.gd
@@ -2,16 +2,21 @@ extends Control
 @onready var traversal: Button = $Traversal
 @onready var mouse: Button = $Mouse
 @onready var wires: Button = $Wires
+@onready var cloude: Button = $Cloude
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	traversal.pressed.connect(toTraversal);
-	pass # Replace with function body.
+	wires.pressed.connect(toWires);
+	mouse.pressed.connect(toMouse);
+	cloude.pressed.connect(toCloude);
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func toTraversal():
 	get_tree().change_scene_to_file("res://Minigames/traversalMinigame/traversalMinigame.tscn")
+func toWires():
+	get_tree().change_scene_to_file("res://Minigames/wiresMinigame/wiresMinigame.tscn")
+func toMouse():
+	get_tree().change_scene_to_file("res://Minigames/mouseMinigame/canvas_layer.tscn")
+func toCloude():
+	get_tree().change_scene_to_file("res://Intro/CloudVideoCutScene/CloudeVideoCutScene.tscn")
