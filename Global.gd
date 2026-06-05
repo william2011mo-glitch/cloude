0d6e87905361d8a3fd9b152b79b5e4293e96de88
williammo29 <william2011mo@gmail.com>
2026-06-05 13:40:59 -0400

    adding globals

---
+++ Global.gd
@@ -0,0 +1,23 @@
+extends Node
+#RUN EVERYTHING GLOBAL HERE
+var paintings = [];
+var money = 0;
+
+var guardRoom = "GrandDrawingRoom";
+var GrandDrawingRoom = [];
+var SmallDrawingRoom = [];
+var MorningRoom = [];
+var PorcelainRoom = [];
+var UpperVestibule = [];
+var LowerVestibule = [];
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
