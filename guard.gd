1f70bfb2438cfc6d2822e06039b011f4305dd557
kyliewelles <kylie@kyliewelles.com>
2026-06-11 00:31:30 -0400

    added all new scenes to movable guard rooms

--- guard.gd
+++ guard.gd
@@ -7,15 +7,24 @@ var money = 0;
 var guardTime = 0.0;
 
 var guardRoom = "GrandDrawingRoom";
-var GrandDrawingRoom = ["Big_DR_enter", "Big_DR_leaving"];
-var SmallDrawingRoom = ["Small_pot_room", "Small_pot_room_right", "Smaller_room_to_small_room", "Small_pot_right_2",
- "Small_pot_right_2", "pot"];
-var MorningRoom = ["Upper_middle_forward_final", "Upper_middle_right_leaving", "Upper_middle_back_1",
- "Upper_middle_forward_1", "Upper_middle_forward_2",
- "Upper_middle_room_opposite", "Upper_middle_right", "Upper_middle_room_turned"];
-var PorcelainRoom = ["case", "Smaller_room"];
-var UpperVestibule = ["Down_stairs_2", "Upper_main", "Upper_door_closer", "chest",
- "Upper_main_left_turn", "Leaving_upper_middle", "Up_stairs_2"];
+var GrandDrawingRoom = ["Big_DR_enter", "Big_DR_leaving", "Big_DR_Left_Piano", "Big_Piano_Close", "Big_Piano_Closer", 
+"Big_Piano_From_Opposite", "Big_DR_golden_baby", "Big_DR_vase", "Big_DR_right"]
+
+var SmallDrawingRoom = ["Small_pot_room", "Small_pot_room_right", "Smaller_room_to_small_room", "Smaller_room_to_small_room_step", 
+"Small_pot_right_2", "Small_pot_right_2", "pot", "Smaller_vase", "Small_room_turned_around", ]
+
+var MorningRoom = ["Upper_middle_room_enter", "Two_boys", "Upper_middle_room_turned",
+	"Leaving_upper_middle", "Upper_middle_forward_1", "Upper_middle_back_1",
+	"Upper_middle_forward_2", "Upper_middle_room_opposite", "Upper_middle_lady",
+	"Upper_middle_right", "Upper_middle_right_leaving", "Upper_middle_right_back",
+	"Upper_middle_jewels", "Upper_middle_forward_final"]
+	
+var PorcelainRoom = ["Smaller_room", "Smaller_room_left", "Smaller_room_left_turned",
+	"case", "Smaller_room_plate", "Smaller_room_plate_turn_around", "Smaller_room_door_leave"]
+	
+var UpperVestibule = ["Upper_main", "chest", "Upper_door_closer", "Upper_main_left_turn",
+	"Upper_main_left_turn_then_around", "Up_stairs_2", "Down_stairs_2"]
+	
 var LowerVestibule = ["Down_stairs_1", "Opening_first", "Opening_right", "Up_stairs_1", "Down_stairs_to_opening"];
 
 
