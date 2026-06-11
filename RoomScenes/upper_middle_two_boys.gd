ecabc08420165a4f8a086143556e32bbab7445ee
kyliewelles <kylie@kyliewelles.com>
2026-06-11 09:27:56 -0400

    smt

--- RoomScenes/upper_middle_two_boys.gd
+++ RoomScenes/upper_middle_two_boys.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-<<<<<<< HEAD
 var minigame_open = false
 
 func _on_boy_thing_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -8,21 +7,17 @@ func _on_boy_thing_input_event(viewport: Node, event: InputEvent, shape_idx: int
 		if minigame_open:
 			return
 		if HeistHUD.has_attempted("two_boys"):
-=======
-var minigame_open := false
-
+			return
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		if minigame_open:
 			return
 		if HeistHUD.has_attempted("two_boys_painting"):
->>>>>>> 6eb8fc09f34396a2141564656eb21a0751d3a47a
 			HeistHUD.show_already_tried_popup()
 			return
 		minigame_open = true
 		HeistHUD.minigame_active = true
 		var mg = load("res://Minigames/colorMinigame/colorMinigame.tscn").instantiate()
-<<<<<<< HEAD
 		add_child(mg)
 		mg.game_finished.connect(_on_color_finished)
 
