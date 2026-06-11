a6bf0cdec2dbfa28c549fa88256a3fa986b2778e
kyliewelles <kylie@kyliewelles.com>
2026-06-11 09:29:19 -0400

    Revert "smt"
    This reverts commit 12f0745c9c13114ebcda6b35d76477e3e9ff74e5.

--- RoomScenes/upper_middle_two_boys.gd
+++ RoomScenes/upper_middle_two_boys.gd
@@ -7,15 +7,12 @@ func _on_boy_thing_input_event(viewport: Node, event: InputEvent, shape_idx: int
 		if minigame_open:
 			return
 		if HeistHUD.has_attempted("two_boys"):
-<<<<<<< HEAD
 			return
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		if minigame_open:
 			return
 		if HeistHUD.has_attempted("two_boys_painting"):
-=======
->>>>>>> parent of 1ed05df (sa)
 			HeistHUD.show_already_tried_popup()
 			return
 		minigame_open = true
@@ -31,3 +28,14 @@ func _on_color_finished(won: bool, _score: int) -> void:
 	if won:
 		HeistHUD.steal_item("the Duo")
 		HeistHUD.mark_attempted("two_boys")
+=======
+		mg.game_finished.connect(_on_color_finished)
+		add_child(mg)
+
+func _on_color_finished(won: bool, _score: int) -> void:
+	minigame_open = false
+	HeistHUD.mark_attempted("two_boys_painting")
+	HeistHUD.minigame_active = false
+	if won:
+		HeistHUD.steal_item("The Painting of Two Boys", 3)
+>>>>>>> 6eb8fc09f34396a2141564656eb21a0751d3a47a
